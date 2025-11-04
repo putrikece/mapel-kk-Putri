d7fdfe021b10afe585886d81edab574fb3000de7
putrikece <putriolaa7@gmail.com>
2025-11-04 12:06:28 +0800

    Add files via upload

--- web-galeri-foto-putribaik/proses/edit_foto.php
+++ web-galeri-foto-putribaik/proses/edit_foto.php
@@ -1,2 +1,9 @@
 <?php
-?>
+session_start ();
+include '../koneksi.php
+
+if (isset($_POST['update'])) {
+   // ambil data dari form
+   $id_foto = intval ($_POST['id_foto']);
+   $judul_foto = mysqli_real_escape_string ($conn, $_POST['judul_foto']);
+   $
